4ff317469e72fc5c26cc41b4133f93ddf8a3131a
Hart Woolery <gohart@gmail.com>
2018-06-10 13:40:41 -0700

    Initial Commit

---
+++ AudioTest/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  AudioTest
+//
+//  Created by Hart Woolery on 6/10/18.
+//  Copyright © 2018 2020CV Inc. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+	override func viewDidLoad() {
+		super.viewDidLoad()
+		// Do any additional setup after loading the view, typically from a nib.
+	}
+
+	override func didReceiveMemoryWarning() {
+		super.didReceiveMemoryWarning()
+		// Dispose of any resources that can be recreated.
+	}
+
+
+}
+
